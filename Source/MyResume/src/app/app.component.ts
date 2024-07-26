c142919a3b20a4dadb998df8b432f7b6747a4a06
saman azadi <samanazadi1996@gmail.com>
2024-07-26 22:58:40 +0330

    Publish in 26-Fri-07_22-58

--- Source/MyResume/src/app/app.component.ts
+++ Source/MyResume/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,11 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'MyResume';
+
+
+  sayHello() {
+    this.toastr.info("Hello ...!","Info");
+  }
+  constructor(private router: Router, private toastr: ToastrService) { }
+
 }
