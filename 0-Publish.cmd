5a82176e055fd8a02ad2894e93280bbfd20c7d63
Saman Azadi <s.azadi@Behpardaz.net>
2024-07-27 10:05:26 +0330

    update publish

--- 0-Publish.cmd
+++ 0-Publish.cmd
@@ -1,15 +1,18 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
-:: Get current date and time
-for /f "tokens=1-4 delims=/ " %%a in ("%date%") do set "date=%%c-%%a-%%b"
-for /f "tokens=1-2 delims=:." %%a in ("%time%") do set "time=%%a-%%b"
-set "datetime=%date%_%time%"
+:: Get the current date in YYYY-MM-DD format
+for /f "tokens=2 delims==" %%i in ('wmic os get localdatetime /value ^| find "="') do set datetime=%%i
+set "year=%datetime:~0,4%"
+set "month=%datetime:~4,2%"
+set "day=%datetime:~6,2%"
+set "hour=%datetime:~8,2%"
+set "minute=%datetime:~10,2%"
 
-:: Format datetime for filename and remove any invalid characters
-set "datetime=%datetime::=-%"
-set "datetime=%datetime:.=-%"
+:: Combine date and time for filename
+set "formatted_datetime=%year%-%month%-%day%_%hour%-%minute%"
 
+echo "Publish on %formatted_datetime%"
 :: Ask the user if they want to publish
 set /p userInput=Do you want to publish changes? (yes/no): 
 
@@ -20,7 +23,7 @@ if /I "%userInput%" == "y" (
     :: Commit and push to git
     echo Committing changes...
     git add .
-    git commit -m "Publish in %datetime%"
+    git commit -m "Publish on %formatted_datetime%"
     if %ERRORLEVEL% NEQ 0 (
         echo Failed to commit changes.
         exit /b 1
